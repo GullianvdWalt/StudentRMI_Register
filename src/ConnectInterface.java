735de95bb8400ac6b123abfdc3c4488cbc536a2b
GullianvdWalt <51962834+GullianvdWalt@users.noreply.github.com>
2020-10-19 16:21:11 +0200

    Updated classes

--- src/ConnectInterface.java
+++ src/ConnectInterface.java
@@ -1,2 +1,13 @@
-public interface ConnectInterface {
+/*
+  Pearson Pretoria
+  BSC IT Level 3 2020
+  ITJA321 - Take Home Assessment
+  Question 5
+  Gullian Van Der Walt - H5G8YT7X3
+ */
+
+import java.rmi.Remote;
+
+public interface ConnectInterface extends Remote {
+  void Insert();
 }
--- src/ConnectionImplement.java
+++ src/ConnectionImplement.java
@@ -1,2 +1,9 @@
+/*
+  Pearson Pretoria
+  BSC IT Level 3 2020
+  ITJA321 - Take Home Assessment
+  Question 5
+  Gullian Van Der Walt - H5G8YT7X3
+ */
 public class ConnectionImplement {
 }
